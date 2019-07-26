6830cb9ef9171ea432c905312d5038a4cb108086
milano12345 <dmilano857@gmail.com>
2019-07-26 10:57:29 -0500

    updated axios call for character and created character card

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard ({props}) {
+  return (<div>
+    <div>Character Name: {props.name}</div>
+    <div>Gender: {props.gender}</div>
+    <div>Species: {props.species}</div>
+    <div>Status: {props.status}</div>
+    </div>)
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [chars, setChars] = useState('')
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios.get('https://rickandmortyapi.com/api/character')
+          .then(res => setChars(res.data.results)) 
+
+      
   }, [])
 
   return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+        <div>{console.log(chars)}</div>
+      
     </section>
 
 }
